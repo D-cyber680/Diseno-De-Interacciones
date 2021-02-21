83bebd85747496b872f2c7475a48b6bd384fd0c5
Daniel Santos Mendez <72686197+D-cyber680@users.noreply.github.com>
2021-02-20 16:12:51 -0800

    Add files via upload

---
+++ Practica2/Assets/Scenes/Problema.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problema : MonoBehaviour
+{
+
+    public static Boolean esta_en_lista(String[] nombres, String nombre)
+    {
+        Boolean esta = false;
+        foreach (String name in nombres)
+        {
+            if (name == nombre)
+            {
+                esta = true;
+                break;
+            }
+            else
+            {
+                esta = false;
+            }
+        }
+        return esta;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boolean x;
+        String[] nombres = { "Abimael", "Araceli", "Brandon", "Cristian", "Daniel",
+            "Edgar","Ivan","Isaac","Nereo"};
+        Debug.Log("Esta Brandon? ");
+        x = esta_en_lista(nombres, "Brandon");
+        if (x)
+        {
+            Debug.Log("Si");
+        }
+        else
+        {
+            Debug.Log("No");
+        }
+    }
+
+  
+    
+}
