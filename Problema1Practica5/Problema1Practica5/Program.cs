37fc11d1f84126bd284320d2080806a5d02b73a5
Daniel Santos Mendez <72686197+D-cyber680@users.noreply.github.com>
2021-03-20 15:01:35 -0700

    Practica 5 Completa

---
+++ Problema1Practica5/Problema1Practica5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Problema1Practica5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {4,1,2,1,2};
+            hallar_elemento_repetido(arr);
+
+        }
+
+        public static bool es_valor_unico(int num, int[] arr)
+        {
+           int  count = 0;
+            for (int i = 0;i < arr.Length; i++)
+            {
+                if (arr[i] == num) 
+                {
+                    count += 1;
+                }
+            }
+            return count == 1;
+        }
+
+        public static void hallar_elemento_repetido(int[] arr)
+        {
+            int no_repetido = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (es_valor_unico(arr[i],arr))
+                {
+                    no_repetido = arr[i];
+                }
+            }
+            Console.WriteLine("El numero que no se repite es: " + no_repetido);          
+        }
+
+
+    }
+}
---
+++ Problema2Practica5/Problema2Practica5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problema2Practica5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {12,345,2,6,7896};
+            cuantos_pares(arr);
+        }
+    public static void cuantos_pares(int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if((arr[i] % 2) == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("La cantidad de numeros pares en arreglo es: " + count);
+    }
+
+    }
+
+    
+}
